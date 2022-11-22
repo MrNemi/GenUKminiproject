ccf9911c90b3984c5a3b3e787c805c160606b8ee
Orion9551 <79426259+Orion9551@users.noreply.github.com>
2022-11-22 10:12:29 +0000

    unit testing

--- Mini-project/mini-project_v4.py
+++ Mini-project/mini-project_v4.py
@@ -17,30 +17,32 @@ load_products(), load_couriers(), load_orders()
 # Main loop
 while True:
     main_menu()
-    # Get user input for main menu option
-    val = int(input('Enter a number to access main menu: '))
-    
-    if val in (0, 1, 2, 3):
-        # If user input is 0, exit app
-        if val == 0:
-            save_products_list(), save_courier_list(), save_order_list()
-            print('All data has been saved.\nLog out successful!!!')
+    try:
+        # Get user input for main menu option
+        val = int(input('Enter a number to access main menu: '))
         
-        # If user input is 1: Enter products menu
-        elif val == 1:
-            product_loop()
+        if val in (0, 1, 2, 3):
+            # If user input is 0, exit app
+            if val == 0:
+                save_products_list(), save_courier_list(), save_order_list()
+                print('All data has been saved.\nLog out successful!!!')
+            
+            # If user input is 1: Enter products menu
+            elif val == 1:
+                product_loop()
 
-        # If user input is 2: Enter couriers menu
-        elif val == 2:
-            couriers_loop()
+            # If user input is 2: Enter couriers menu
+            elif val == 2:
+                couriers_loop()
 
-        # If user input is 3: Enter orders menu
-        elif val == 3:
-            orders_loop()
-        
-        menu_continue = input("Proceed to the main menu? (yes/no): ")
-        if menu_continue == "no":
-          break
-
-    else:
-        print("Invalid input. Enter either a 0 or 1")
+            # If user input is 3: Enter orders menu
+            elif val == 3:
+                orders_loop()
+            
+            menu_continue = input("Proceed to the main menu? (yes/no): ")
+            if menu_continue == "no":
+                break
+        else:
+            print("Invalid input. Enter either a 0 or 1")
+    except ValueError as e:
+        print(e)
