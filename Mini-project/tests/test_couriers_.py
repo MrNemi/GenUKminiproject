9ec9785388e3acd312d0ea56133ef8c82df6ee4c
Orion9551 <79426259+Orion9551@users.noreply.github.com>
2022-11-23 01:40:01 +0000

    unit tests

---
+++ Mini-project/tests/test_couriers_.py
@@ -0,0 +1,11 @@
+from functions.couriers_ import new_couriers
+
+
+# def test_new_couriers():
+
+
+#     expected = " "
+#     actual = new_couriers()
+
+#     assert expected == actual
+#     print("test_get_country_currency: Passed")
---
+++ Mini-project/tests/test_csvfile_handling.py
@@ -0,0 +1,11 @@
+from file_handling.csvfile_handling import load_products, load_couriers, load_orders
+
+# def test_load_products():
+
+
+
+# def test_load_couriers():
+
+
+
+# def test_load_orders():
